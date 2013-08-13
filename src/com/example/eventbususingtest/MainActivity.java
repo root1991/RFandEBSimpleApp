0105eab66f40bf6beeb8b88914130ec80546b637
root <anhristyan@gmail.com>
2013-08-13 15:13:55 +0300

    second commit

--- src/com/example/eventbususingtest/MainActivity.java
+++ src/com/example/eventbususingtest/MainActivity.java
@@ -5,6 +5,7 @@ import com.example.eventbususingtest.events.RetrofitErrorEvent;
 import de.greenrobot.event.EventBus;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 
@@ -33,9 +34,17 @@ public abstract class MainActivity extends Activity {
 		EventBus.getDefault().registerSticky(this);
 	}
 
-	public void onEventMainThread(
-			RetrofitErrorEvent event) {
-		Log.e("RETROFIT ERROR", "ERROR");
+	public void onEventMainThread(RetrofitErrorEvent event) {
+		String errText = "network";
+		if (event.getError() == null || event.getError().isNetworkError()) {
+			errText = "network";
+		} else if (event.getError().getResponse() != null
+				&& event.getError().getResponse().getReason() != null) {
+			errText = event.getError().getResponse().getReason().toUpperCase();
+		} else if (!TextUtils.isEmpty(event.getError().getMessage())) {
+			errText = event.getError().getMessage().toUpperCase();
+		}
+		Log.e("RETROFIT ERROR", errText);
 
 	}
 
--- src/com/example/eventbususingtest/ResultActivity.java
+++ src/com/example/eventbususingtest/ResultActivity.java
@@ -1,13 +1,10 @@
 package com.example.eventbususingtest;
 
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.eventbususingtest.callbacks.JSONRequest;
 import com.example.eventbususingtest.events.RequestEvent;
-import com.example.eventbususingtest.model.RequestModel;
 import com.example.eventbususingtest.rest.RestAdapterCreator;
 
 public class ResultActivity extends MainActivity{
@@ -17,23 +14,13 @@ public class ResultActivity extends MainActivity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		
+		creatror.getmJSONInterFace().getJSON("title", new JSONRequest());
 	}
+	
+	
 
 	public void onEventMainThread(RequestEvent event) {
-		creatror.getmJSONInterFace().getJSON("title", new Callback<RequestModel>() {
-
-			@Override
-			public void success(RequestModel t, Response response) {
-				Log.d("REQUEST", t.title);
-			}
-
-			@Override
-			public void failure(RetrofitError error) {
-				Log.e("FAILURE", "fail");
-			}
-		});
+		Log.d("REQUEST", event.getModel().title);
 	}
 	
 	
